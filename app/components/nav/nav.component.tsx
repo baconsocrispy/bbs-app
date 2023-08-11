b452b5a37eb54010c6a2716aad1bbf26ca46b27a
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-08-11 10:47:08 -0700

    added signin Link component to Nav bar

--- app/components/nav/nav.component.tsx
+++ app/components/nav/nav.component.tsx
@@ -1,9 +1,21 @@
+'use client'
+
+// library
+import { useContext } from "react";
+import Link from "next/link";
+
+// context
+import { UserContext } from "@/app/contexts/user.context";
+
 const Nav = () => {
+  // state
+  const { user } = useContext(UserContext);
+
   return (
     <div className="nav">
-      Nav
+      { user ? 'Log Out' : <Link href='/signin'>Sign In</Link> }
     </div>
   )
 }
 
-export default Nav
+export default Nav;
