1420992e427921af4712fa0fd1f20d6d35b83e8f
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-08-25 14:04:11 -0700

    created products-api and moved auth helpers to api-helpers

--- app/api/api-helpers.ts
+++ app/api/api-helpers.ts
@@ -1,17 +1,65 @@
-// send a JWT-encrypted request to backend
-export const backendJWTRequest = async (
+// types
+import { AuthFormData } from "../components/auth-form/auth-form.component";
+
+
+// HELPERS
+// send auth form data in backend request
+// when backend/frontend hosted on same domain, 
+// change credentials to 'same-site'
+export const backendAuthRequest = async (
   method: string,
-  url: string | URL,
-  jwt: string | null,
-  data?: string
+  url: string,
+  data: AuthFormData | null = null
 ) => {
   const response = await fetch(url, {
+    credentials: 'include',
     method: method,
     headers: {
-      'Authorization': `Bearer ${ jwt }`,
-      'Content-Type': 'application/json'
+      'Authorization': `Basic ${ doorkeeperCredentials() }`,
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: JSON.stringify(data)
+    body: formatFormData(data)
   });
-  return response.json();
+  return response;
+};
+
+// convert form data into URLSearchParams format
+const formatFormData = (data: AuthFormData | null) => {
+  if (!data) return;
+
+  // url encode form data
+  const params = new URLSearchParams();
+
+  // doorkeeper config
+  params.append('grant_type', process.env.NEXT_PUBLIC_DOORKEEPER_GRANT_TYPE as string);
+
+  // user auth config
+  params.append('email', data.user.email);
+  params.append('password', data.user.password);
+
+  if (data.user.firstName) { 
+    params.append('firstName', data.user.firstName) 
+  }
+
+  if (data.user.lastName) { 
+    params.append('lastName', data.user.lastName) 
+  }
+
+  if (data.user.passwordConfirmation) { 
+    params.append('passwordConfirmation', data.user.passwordConfirmation) 
+  }
+
+  return params;
+};
+
+// encode doorkeeper credentials in base64 format
+const doorkeeperCredentials = () => {
+  // doorkeeper credentials
+  const clientId = process.env.NEXT_PUBLIC_DOORKEEPER_CLIENT_ID;
+  const clientSecret = process.env.NEXT_PUBLIC_DOORKEEPER_SECRET;
+
+  // encodes credentials in base64 format
+  const credentials = btoa(`${ clientId }:${ clientSecret }`);
+
+  return credentials;
 };
