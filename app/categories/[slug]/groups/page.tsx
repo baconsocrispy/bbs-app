b8d1da07a2fa20e7f02c71a83e4c0a6c3e4d0880
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-09-04 16:30:50 -0700

    created groups page component

---
+++ app/categories/[slug]/groups/page.tsx
@@ -0,0 +1,12 @@
+// components
+import Header from "../../../components/header/header.component";
+
+const GroupsPage = () => {
+  return (
+    <main>
+      <Header text='Groups Page' imageUrl='/architecture.jpeg'/>
+    </main>
+  )
+};
+
+export default GroupsPage;
