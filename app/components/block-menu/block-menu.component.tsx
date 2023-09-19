a921ffa7eace4327e0dbcddf3b1db79cda9d53b8
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-09-19 10:12:01 -0700

    added edit buttons to categories

--- app/components/block-menu/block-menu.component.tsx
+++ app/components/block-menu/block-menu.component.tsx
@@ -1,13 +1,23 @@
 'use client'
 
 // library
-import { FC, MouseEventHandler, useEffect, useState } from "react";
+import { 
+  FC, 
+  MouseEventHandler, 
+  useContext,
+  useEffect, 
+  useState 
+ } from "react";
+
 import { usePathname } from "next/navigation";
 
 // components
 import Image from "next/image";
 import Link from "next/link";
 import Spinner from "../spinner/spinner.component";
+
+// context
+import { UserContext } from "@/app/contexts/user.context";
 
 // types
 import { Category } from "@/app/api/api-types";
@@ -23,6 +33,7 @@ const BlockMenu: FC<BlockMenuProps> = ({
   open, menuItems, path, onClick 
 }) => {
   // state
+  const user = useContext(UserContext);
   const pathname = usePathname();
   const [ loading, setLoading ] = useState(true);
   const [ imagesLoaded, setImagesLoaded ] = useState(0);
@@ -41,59 +52,70 @@ const BlockMenu: FC<BlockMenuProps> = ({
 
   return (
     <>
-       { loading && <Spinner />}
-        <nav 
-          className={ open ? 'block-menu' : 'block-menu--closed' }
-        >
-        
-          { onClick && 
-            <button 
-              className='block-menu__close'
-              onClick={ onClick }
-            >
-              X
-            </button>
-          }
+      { loading && <Spinner /> }
+      <nav 
+        className={ open ? 'block-menu' : 'block-menu--closed' }
+      >
+      
+        { onClick && 
+          <button 
+            className='block-menu__close'
+            onClick={ onClick }
+          >
+            X
+          </button>
+        }
 
-          <ul className='block-menu__items'>
-            { menuItems?.map((item) => {
-              return (
-                <li 
-                  key={ item.id } 
-                  className="block-menu__item" 
-                >
-                  <Link 
-                    className="block-menu__link" 
-                    href={ path ? 
-                      (path + `/${ item.slug }`) : 
-                      (pathname + `/${ item.slug }`) 
-                    }
-                  > 
-                    <Image
-                      className="block-menu__image"
-                      onLoadingComplete={ () => onLoadHandler() }
-                      src={ item.image.url }
-                      alt={ item.name }
-                      fill
-                    />
+        <ul className='block-menu__items'>
+          { menuItems?.map((item) => {
+            return (
+              <li 
+                key={ item.id } 
+                className="block-menu__item" 
+              >
+                { user && 
+                  <button className="block-menu__edit-button">
+                    <Link 
+                      className="block-menu__edit-link"
+                      href={ `/categories/edit/${ item.slug }` }
+                    >
+                      Edit
+                    </Link>
+                  </button>
+                }
 
-                    <div className="block-menu__header-container">
-                      <h4 className="block-menu__header">
-                        { item.name }
-                      </h4>
-                    </div>
-                    
-                    <div className="block-menu__text-container">
-                      <p className="block-menu__text">
-                        { item.short_description }
-                      </p>
-                    </div>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
+                <Link 
+                  className="block-menu__link" 
+                  href={ path ? 
+                    (path + `/${ item.slug }`) : 
+                    (pathname + `/${ item.slug }`) 
+                  }
+                > 
+                  <Image
+                    className="block-menu__image"
+                    onLoadingComplete={ () => onLoadHandler() }
+                    src={ item.image.url }
+                    alt={ item.name }
+                    fill
+                  />
+
+                  <div className="block-menu__header-container">
+                    <h4 className="block-menu__header">
+                      { item.name }
+                    </h4>
+                  </div>
+                  
+                  <div className="block-menu__text-container">
+                    <p className="block-menu__text">
+                      { item.short_description }
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
     </>
   )
 }
