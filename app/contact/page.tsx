2f364d067654923d62784080a70b79f47dec6629
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-09-28 14:01:50 -0700

    added header to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,11 @@
 // components
 import ContactForm from "../components/contact-form/contact-form.component";
+import Header from "../components/header/header.component";
 
 const Contact = () => {
   return (
     <main className="contact-page">
+      <Header imageUrl={ '/contact-banner-1.png' } text='Contact Us'/>
       <ContactForm />
     </main>
   )
