99dd317cf96dea537cf81b609e2c612e712a0374
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-10-13 12:57:36 -0700

    fixed RequestCookie issue in auth route

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -18,7 +18,7 @@ export const GET = async () => {
 export const POST = async (request: Request) => {
   // extract doorkeeper auth token from cookies
   const cookieStore = cookies();
-  const token = cookieStore.get('access_token');
+  const token = cookieStore.get('access_token')?.value;
 
   // get form data from request
   const formData = await request.formData();
