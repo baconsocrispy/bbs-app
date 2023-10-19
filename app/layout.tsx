b7394f3a6d7885d2858b3d663e5cdceb761bcd02
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-10-19 09:26:42 -0700

    added styling per client call

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Grid>
-          <Banner />
           <UserProvider>
             <Nav />
             { children }
