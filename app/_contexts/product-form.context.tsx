c1af7f1c3d51cb1553f6528dc457e051eebbea2a
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-11-01 12:55:03 -0700

    added Feature actions to product-form-context

--- app/_contexts/product-form.context.tsx
+++ app/_contexts/product-form.context.tsx
@@ -2,10 +2,16 @@
 import { createContext, ReactNode, useState } from "react";
 
 // types
-import {  } from "../api/api-types";
+import { Feature, Spec, TextBlock } from "../api/api-types";
+
+type ProductFormOptions = {
+  features: Feature[];
+  specs: Spec[];
+  textBlocks: TextBlock[];
+};
 
 type ProductFormContextProps = {
-
+  formOptions: ProductFormOptions;
 };
 
 type ProductFormProviderProps = {
@@ -13,18 +19,48 @@ type ProductFormProviderProps = {
 }
 
 // context
-export const ProductFormContext = createContext<ProductFormContextProps>({});
+export const ProductFormContext = createContext<ProductFormContextProps>({
+  formOptions: {
+    features: [],
+    specs: [],
+    textBlocks: []
+  }
+});
 
 // provider
 export const ProductFormProvider = ({ children }: ProductFormProviderProps) => {
   // initial state
-  const emptyForm = {};
+  const emptyForm: ProductFormOptions = {
+    features: [],
+    specs: [],
+    textBlocks: []
+  };
 
   const [ formOptions, setFormOptions ] = useState(emptyForm);
 
   // actions
   const resetForm = () => {
     setFormOptions(emptyForm);
+  };
+
+  const addFeature = (feature: Feature) => {
+    const updatedFormOptions = { ...formOptions };
+    updatedFormOptions.features.push(feature);
+    setFormOptions(updatedFormOptions);
+  };
+
+  const removeFeature = (index: number) => {
+    const updatedFormOptions = { ...formOptions };
+    updatedFormOptions.features.splice(index, 1);
+    setFormOptions(updatedFormOptions);
+  };
+
+  const updateFeature = (feature: Feature, index: number) => {
+    const updatedFormOptions = { ...formOptions };
+    updatedFormOptions.features[index] = {
+      ...feature
+    };
+    setFormOptions(updatedFormOptions);
   };
 
   // export data
