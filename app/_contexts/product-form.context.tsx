5a4904653cbcd2aa22891a8d2236de5bb23e568f
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-11-01 12:20:02 -0700

    added ProductFormProvider

--- app/_contexts/product-form.context.tsx
+++ app/_contexts/product-form.context.tsx
@@ -3,3 +3,39 @@ import { createContext, ReactNode, useState } from "react";
 
 // types
 import {  } from "../api/api-types";
+
+type ProductFormContextProps = {
+
+};
+
+type ProductFormProviderProps = {
+  children: ReactNode;
+}
+
+// context
+export const ProductFormContext = createContext<ProductFormContextProps>({});
+
+// provider
+export const ProductFormProvider = ({ children }: ProductFormProviderProps) => {
+  // initial state
+  const emptyForm = {};
+
+  const [ formOptions, setFormOptions ] = useState(emptyForm);
+
+  // actions
+  const resetForm = () => {
+    setFormOptions(emptyForm);
+  };
+
+  // export data
+  const value = {
+    formOptions,
+    resetForm
+  };
+
+  return (
+    <ProductFormContext.Provider value={ value }>
+      { children }
+    </ProductFormContext.Provider>
+  );
+};
