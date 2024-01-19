7492a47b257e8621eabb1c8e4ca1c4d8dab181c4
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2024-01-19 13:45:01 -0800

    added phone number to footer

--- app/_components/footer/footer.component.tsx
+++ app/_components/footer/footer.component.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      &copy;&nbsp;2023 BB&S America
+      <div><p>&copy;&nbsp;2023 BB&S America</p></div>
+      
+      <div className="contact-number"><a href="tel:18008206610"><p>+1 (800) 820-6610</p></a></div>
     </footer>
   )
 }
